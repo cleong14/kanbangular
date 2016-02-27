b03a73b5d3b77c249bd889c00b7f0b87a278e87c
Chaz Leong <cleong14@gmail.com>
2016-02-27 09:52:35 -1000

    Added basic express server and displaying basic index.html

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.resolve(__dirname + '/public')));
+
+var server = app.listen(3000, function() {
+  console.log('Listening to port', server.address().port);
+});
